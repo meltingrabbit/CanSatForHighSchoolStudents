cf1a92ff9653def22a4f7e8a2ba0f2ddb8508e0c
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2020-02-21 15:04:41 +0900

    コメント追加

--- Arduino/ProgrammingTtutorial01_HelloWorld/ProgrammingTtutorial01_HelloWorld.ino
+++ Arduino/ProgrammingTtutorial01_HelloWorld/ProgrammingTtutorial01_HelloWorld.ino
@@ -1,16 +1,16 @@
-void setup()
+void setup()                              // 最初に実行される
 {
-	Serial.begin(9600);
-	Serial.println("Hello World!!");
-	delay(1000);
-	Serial.println("Setup!");
-	delay(1000);
+  Serial.begin(9600);                     // 通信速度 9600 bps で通信開始
+  Serial.println("Hello World!!");        // 文字を出力
+  delay(1000);                            // 1000 ms 待つ
+  Serial.println("Setup!");
+  delay(1000);
 }
 
-void loop()
+void loop()                               // Setup後，繰り返し実行される
 {
-	Serial.println("Loop!");
-	// Serial.println("Comment");
-	delay(1000);
+  Serial.println("Loop!");
+  // Serial.println(“Comment”);         // コメント行（実行されない）
+  delay(1000);
 }
 
--- Arduino/ProgrammingTtutorial02_Variable/ProgrammingTtutorial02_Variable.ino
+++ Arduino/ProgrammingTtutorial02_Variable/ProgrammingTtutorial02_Variable.ino
@@ -1,17 +1,17 @@
 void setup()
 {
-	Serial.begin(9600);
-	String str = "String"
-	Serial.print("String: ");
-	Serial.println(str);
+  Serial.begin(9600);
+  String str = "String"
+  Serial.print("String: ");
+  Serial.println(str);
 
-	int i = 10;
-	Serial.print("Int: ");
-	Serial.println(i);
+  int i = 10;
+  Serial.print("Int: ");
+  Serial.println(i);
 
-	float f = 10;
-	Serial.print("Float: ");
-	Serial.println(f);
+  float f = 10;
+  Serial.print("Float: ");
+  Serial.println(f);
 }
 
 void loop()
