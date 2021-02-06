c0f135a451c5debe50ccb4149d7e6f09462aa14b
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2021-02-07 02:12:04 +0900

    add cam code

---
+++ Arduino/Test_Camera2/Test_Camera2.ino
@@ -0,0 +1,24 @@
+#include "./SD.h"
+#include "./Camera2.h"
+
+
+void setup()
+{
+	// Wire(Arduino-I2C)の初期化
+	Wire.begin();
+	// デバック用シリアル通信は9600bps
+	Serial.begin(9600);
+
+	// 初期化
+	SD_Init();		// これは絶対最初に初期化！
+	CAM2_Init();		// SDの後！
+
+	Serial.println(F("Init done"));
+	delay(300);
+}
+
+void loop()
+{
+	CAM2_TakePic();		// 写真を撮る
+	delay(1000);			// 1000 ms 待つ
+}
