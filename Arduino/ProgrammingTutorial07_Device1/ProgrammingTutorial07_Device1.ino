7bcebd0820c5ac0f58e836811b53fbd7f3c64ace
Toshio Imamura <periodqwerty1@gmail.com>
2021-02-07 14:53:27 +0900

    modify outputs

--- Arduino/ProgrammingTutorial07_Device1/ProgrammingTutorial07_Device1.ino
+++ Arduino/ProgrammingTutorial07_Device1/ProgrammingTutorial07_Device1.ino
@@ -1,12 +1,13 @@
 // 気圧計のライブラリ（便利なコードまとめ）を使うよ！ と宣言
-#include "./Barometer.h"
+#include "./BarometerThermohygrometer.h"
 // すると，
-// Barometer.h に宣言されている
-//   void BMP_Init();
-//   void BMP_Update();
-//   void BMP_Print();
-//   float BMP_GetPressure();
-//   float BMP_GetTemperature();
+// BarometerThrmohygrometer.h に宣言されている
+//  void BTH_Init();
+//  void BTH_Update();
+//  void BTH_Print();
+//  float BTH_GetPressure();
+//  float BTH_GetHumidity();
+//  float BTH_GetTemperature();
 // という関数が使えるようになる
 // 関数の中身は Barometer.c にかかれている
 
@@ -19,7 +20,7 @@ void setup()
   Serial.begin(9600);
 
   // 気圧計を初期化
-  BMP_Init();
+  BTH_Init();
 
   Serial.println(F("Init done"));
   delay(300);
@@ -28,24 +29,18 @@ void setup()
 void loop()
 {
   // 気圧計の値を更新
-  BMP_Update();
+  BTH_Update();
   // // 気圧計の値を表示
-  // BMP_Print();
-
-  // 気圧計の値を取得
-  float pressure = BMP_GetPressure();
-  Serial.print(F("Pressure: "));
-  Serial.print(pressure);
-  Serial.println(F(" [Pa]"));
+  BTH_Print();
+  float pressure = BTH_GetPressure();
 
   // 気圧から高度に変換
   // まあ，ざっくり 10m上昇すると1hPa下がるとしようか．
-  float pressure_at_sea_level = 101325.0;
-  float height = (pressure_at_sea_level - pressure) / 100 * 10;
+  float pressure_at_sea_level = 1013.250;
+  float height = (pressure_at_sea_level - pressure) * 10;
 
-  Serial.print(F("Height: "));
+  Serial.print(F("Heig = "));
   Serial.print(height);
   Serial.println(F(" [m]"));
-
   delay(1000);
 }
