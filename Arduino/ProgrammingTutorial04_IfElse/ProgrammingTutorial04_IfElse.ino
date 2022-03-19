e8467b1fb22491cc5164cbb8b5b74b0af6adb75b
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2022-03-20 03:28:04 +0900

    fix if else code

--- Arduino/ProgrammingTutorial04_IfElse/ProgrammingTutorial04_IfElse.ino
+++ Arduino/ProgrammingTutorial04_IfElse/ProgrammingTutorial04_IfElse.ino
@@ -2,43 +2,47 @@ void setup()
 {
   Serial.begin(9600);
 
-  int a = 10;                     // aに整数10を代入
+  int a = 10;                         // 整数型 (int) の変数 a に整数 10 を代入
 
-  if (a == 10) {                  // aは10？
-    Serial.println("a == 10");
-  } else {                        // それ以外
-    Serial.println("otherwise");
+  Serial.println("a == 10 ? :");
+  if (a == 10) {                      // a は 10 ？
+    Serial.println("  -> a == 10");
+  } else {                            // それ以外
+    Serial.println("  -> otherwise");
   }
 
-  if (a != 10) {                  // aは10ではない？
-    Serial.println("a != 10");
-  } else {                        // それ以外
-    Serial.println("otherwise");
+  Serial.println("a != 10 ? :");
+  if (a != 10) {                      // a は 10 ではない？
+    Serial.println("  -> a != 10");
+  } else {                            // それ以外
+    Serial.println("  -> otherwise");
   }
 
-  if (a > 10) {                   // aは10より大きい？
-    Serial.println("a > 10");
-  } else if (a >= 10) {           // aは10より大きくはないけど，10以上？
-    Serial.println("a >= 10");
-  } else {                        // それ以外
-    Serial.println("otherwise");
+  Serial.println("a > 10 or a >= 10 ? :");
+  if (a > 10) {                       // a は 10 より大きい？
+    Serial.println("  -> a > 10");
+  } else if (a >= 10) {               // a は 10 より大きくはないけど， 10 以上？
+    Serial.println("  -> a >= 10");
+  } else {                            // それ以外
+    Serial.println("  -> otherwise");
   }
 
 
-  if ((a > 5) && (a < 20)) {      // aは5より大きく，かつ，20より小さい？
-    Serial.println("(a > 5) && (a < 20)");
-  } else {                        // それ以外
-    Serial.println("otherwise");
+  Serial.println("(a > 5) && (a < 20) ? :");
+  if ((a > 5) && (a < 20)) {          // a は 5 より大きく，かつ， 20 より小さい？
+    Serial.println("  -> (a > 5) && (a < 20)");
+  } else {                            // それ以外
+    Serial.println("  -> otherwise");
   }
 
-  if ((a > 5) || (a > 20)) {      // aは5より大きい，または，20より大きい？
-    Serial.println("(a > 5) || (a > 20)");
-  } else {                        // それ以外
-    Serial.println("otherwise");
+  Serial.println("(a > 5) || (a > 20) ? :");
+  if ((a > 5) || (a > 20)) {          // a は 5 より大きい，または， 20 より大きい？
+    Serial.println("  -> (a > 5) || (a > 20)");
+  } else {                            // それ以外
+    Serial.println("  -> otherwise");
   }
 }
 
 void loop()
 {
 }
-
