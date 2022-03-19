43c6ee6791cf3e4e9d3a1e1fdc9234d889455c7c
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2022-03-20 04:15:44 +0900

    add code of divece3

--- Arduino/ProgrammingTutorial09_Device3/ProgrammingTutorial09_Device3.ino
+++ Arduino/ProgrammingTutorial09_Device3/ProgrammingTutorial09_Device3.ino
@@ -1,38 +1,25 @@
-// ９軸センサのライブラリ（便利なコードまとめ）を使うよ！ と宣言
-#include "./IMU.h"
+// サーボモータのライブラリ（便利なコードまとめ）を使うよ！ と宣言
+#include "./Servo.h"
 // すると，
-// IMU.h に宣言されている
-//   void IMU_Init();
-//   void IMU_UpdateAll();
-//   void IMU_UpdateAcc();
-//   void IMU_UpdateGyr();
-//   void IMU_UpdateMag();
-//   void IMU_PrintAcc();
-//   void IMU_PrintGyr();
-//   void IMU_PrintMag();
-//   float IMU_GetAccX();
-//   float IMU_GetAccY();
-//   float IMU_GetAccZ();
-//   float IMU_GetGyrX();
-//   float IMU_GetGyrY();
-//   float IMU_GetGyrZ();
-//   int16_t IMU_GetMagX();
-//   int16_t IMU_GetMagY();
-//   int16_t IMU_GetMagZ();
+// Servo.h に宣言されている
+//   void SRV_Init();
+//   void SRV_SetPosition(uint8_t angle);
+//   uint8_t SRV_GetPosition();
+//   void SRV_Run();
+//   void SRV_Detach();
+//   void SRV_Attach();
 // という関数が使えるようになる
-// 関数の説明は IMU.h に書かれている
-// 関数の中身は IMU.ino に書かれている
+// 関数の説明は Servo.h に書かれている
+// 関数の中身は Servo.ino に書かれている
 
+uint16_t loop_count = 0;      // プログラムのどこからでも使える変数 loop_count を準備しておく
 
 void setup()
 {
-  // Wire(Arduino-I2C) の初期化．気圧計との通信を開始
-  Wire.begin();
   // デバック用シリアル通信は 9600bps
   Serial.begin(9600);
 
-  // 9軸センサを初期化
-  IMU_Init();
+  SRV_Init();		// サーボモータを初期化
 
   Serial.println(F("Init done"));
   delay(300);
@@ -40,23 +27,37 @@ void setup()
 
 void loop()
 {
-  IMU_UpdateAll();    // 9軸センサの値をアップデート
-  // IMU_PrintAcc();     // 取得した加速度を表示
-  // IMU_PrintGyr();     // 取得した角加速度を表示
-  // IMU_PrintMag();     // 取得した磁気を表示
+  // 今何周目のループか表示
+  Serial.println(loop_count);
 
-  // x, y の地磁気の値を取得
-  int mag_x = IMU_GetMagX();
-  int mag_y = IMU_GetMagY();
+  // ループカウンタが10の倍数のときに実行
+  if (loop_count % 10 == 0) {
+    Serial.println(F("Move Up!"));
+    // 0 ~ 180 度に， 30 度ずつ， 0.5秒ごとに回転させる
+    for(uint8_t angle = 0; angle <= 180; angle = angle + 30) {
+      Serial.print(F("angle:"));
+      Serial.println(angle);
+      SRV_SetPosition(angle);
+      SRV_Run();
+      delay(500);       // 500 ms （0.5秒）待つ
+    }
+  }
 
-  // x, y の地磁気から，北からの方位を求める．
-  // なお，本来は地磁気の xy 平面が描く楕円を原点を中心とした真円になおすキャリブレーションが必要
-  float magnet_radian = atan2((float)mag_y, (float)mag_x);
-  float magnet_degree = magnet_radian * 180.0 / PI;
+  // ループカウンタを10で割った剰余が5のときに実行
+  if (loop_count % 10 == 5) {
+    Serial.println(F("Move Down!"));
+    // 180 - 0 度に， 1 度ずつ， 0.02秒ごとに回転させる
+    // 0.02秒と人間にとっては短い時間なので，なめらかにゆっくり回転するように見える
+    for(uint8_t angle = 180; angle > 0; angle--) {
+      Serial.print(F("angle:"));
+      Serial.println(angle);
+      SRV_SetPosition(angle);
+      SRV_Run();
+      delay(20);        // 20 ms （0.02秒）待つ
+    }
+  }
 
-  Serial.print(F("Direction: "));
-  Serial.print(magnet_degree);
-  Serial.println(F(" [deg]"));
-
-  delay(250);
+  // ループカウントを 1 増やす
+  loop_count++;
+  delay(1000);
 }
--- Arduino/ProgrammingTutorial09_Device3/README.md
+++ Arduino/ProgrammingTutorial09_Device3/README.md
@@ -7,26 +7,86 @@
 
 ## ソースコード
 ```cpp
+// サーボモータのライブラリ（便利なコードまとめ）を使うよ！ と宣言
+#include "./Servo.h"
+// すると，
+// Servo.h に宣言されている
+//   void SRV_Init();
+//   void SRV_SetPosition(uint8_t angle);
+//   uint8_t SRV_GetPosition();
+//   void SRV_Run();
+//   void SRV_Detach();
+//   void SRV_Attach();
+// という関数が使えるようになる
+// 関数の説明は Servo.h に書かれている
+// 関数の中身は Servo.ino に書かれている
 
+uint16_t loop_count = 0;      // プログラムのどこからでも使える変数 loop_count を準備しておく
 
+void setup()
+{
+  // デバック用シリアル通信は 9600bps
+  Serial.begin(9600);
+
+  SRV_Init();		// サーボモータを初期化
+
+  Serial.println(F("Init done"));
+  delay(300);
+}
+
+void loop()
+{
+  // 今何周目のループか表示
+  Serial.println(loop_count);
+
+  // ループカウンタが10の倍数のときに実行
+  if (loop_count % 10 == 0) {
+    Serial.println(F("Move Up!"));
+    // 0 ~ 180 度に， 30 度ずつ， 0.5秒ごとに回転させる
+    for(uint8_t angle = 0; angle <= 180; angle = angle + 30) {
+      Serial.print(F("angle:"));
+      Serial.println(angle);
+      SRV_SetPosition(angle);
+      SRV_Run();
+      delay(500);       // 500 ms （0.5秒）待つ
+    }
+  }
+
+  // ループカウンタを10で割った剰余が5のときに実行
+  if (loop_count % 10 == 5) {
+    Serial.println(F("Move Down!"));
+    // 180 - 0 度に， 1 度ずつ， 0.02秒ごとに回転させる
+    // 0.02秒と人間にとっては短い時間なので，なめらかにゆっくり回転するように見える
+    for(uint8_t angle = 180; angle > 0; angle--) {
+      Serial.print(F("angle:"));
+      Serial.println(angle);
+      SRV_SetPosition(angle);
+      SRV_Run();
+      delay(20);        // 20 ms （0.02秒）待つ
+    }
+  }
+
+  // ループカウントを 1 増やす
+  loop_count++;
+  delay(1000);
+}
 ```
 
-+ 缶サットを回転させ，方位が変わることを確認しよう
-+ 他の機器でも同じようなプログラムを作ってみよう
++ `dalay` などを変更し，回転する速度を変えてみよう
++ 回転角を変えてみよう
 
 
 ## ピンアサイン（ピン配置）
 + `PIN_ASSIGN.h` というファイルで，マイコンのどのピンにどこを接続するかを定義している．
-+ たとえば，I2Cで使うピンである， SDA, SCL を定義してもよいが，ここではデフォルト値（初期値）を使っているため，特に記述してない（コメントアウトしている）．
++ たとえば，ここではサーボモータに回転角を司令する信号を4番ピンに指定している
 ```cpp
-// #define SDA A4
-// #define SCL A5
+#define PIN_SRV 4
 ```
 + 適切にピンアサインを変更することで，複数の機器を同時に接続したり，回路の設計を簡単にすることができる．
 
 
 ## 備考
-+ x軸とy軸のセンサ出力には，個体差があるため，もし，精度の良い方位磁石を作る場合は，事前に調整（キャリブレーション）が必要．
++ `hoge++` とは， 変数 `hoge` の値を 1 増やす． `hoge--` とは， 変数 `hoge` の値を 1 減らす．
 + ここでは，講義の時間の都合上，用意された関数群を用いた．
 + 本来であれば，センサに応じたサンプルコードをインターネットなどからダウンロードすると良い．
 	- 「センサ名 Arduino サンプルコード」などでGoogle検索をかけるとよい．
@@ -38,5 +98,5 @@
 
 
 ## ソースコードへのリンク
-[GitHub](https://github.com/meltingrabbit/CanSatForHighSchoolStudents/tree/master/Arduino/ProgrammingTutorial08_Device2)
+[GitHub](https://github.com/meltingrabbit/CanSatForHighSchoolStudents/tree/master/Arduino/ProgrammingTutorial09_Device3)
 
